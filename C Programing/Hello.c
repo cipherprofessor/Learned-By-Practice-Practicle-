67831b2a27383e3ef5437eabfc923eb43871d228
Cipher Professor <mohsinmanzoor1913@gmail.com>
2023-01-17 01:05:15 +0530

    17 jan

--- C Programing/Hello.c
+++ C Programing/Hello.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 int main() {
+
    // printf() displays the string inside quotation
    printf("Hello World , Let's Begin From The Beginning");
    printf("Hello Mohsin");  //printf use to print anuthing
+   printf("My first Program ")
    /* MOhsin
    Tazafat
    Sadiya
     */
+   int i =3;
+   scanf(%d,&i);
    return 0;
 }
 
